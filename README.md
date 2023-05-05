6eb5be1cfbf0bc6b821020fa3f1341ce36079564
JuanManuelGarciaUTN <artlixUTN@gmail.com>
2023-05-04 21:16:06 -0300

    Agrego firestorage y firebase hosting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tp-sala-de-juegos
 Garcia Juan Manuel - 4°B - Laboratorio IV - TP Sala de Juegos
+# Deploy link: https://garcia-juan-manuel-sala-de-juegos.firebaseapp.com/
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'tp-sala-de-juegos-3ac5e',
+    appId: '1:966154419731:web:341bd86326a5385d9109e6',
+    storageBucket: 'tp-sala-de-juegos-3ac5e.appspot.com',
+    apiKey: 'AIzaSyBuR2-6sks8v8xWAEiVoaxHw1tj0pY2M2M',
+    authDomain: 'tp-sala-de-juegos-3ac5e.firebaseapp.com',
+    messagingSenderId: '966154419731',
+  },};
